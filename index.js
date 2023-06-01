046f4b7d5edac5e8289fb47bb086b627fd352f23
cwchilvers <cwchilvers@gmail.com>
2023-05-31 19:10:39 -0700

    Added inquirer questions

--- index.js
+++ index.js
@@ -1,2 +1,52 @@
 const inquirer = require('inquirer');
 
+inquirer 
+    .prompt([
+        {
+        type: 'input',
+        message: 'Project title:',
+        name: 'title',
+        },
+        {
+        type: 'input',
+        message: 'Project description:',
+        name: 'description',
+        },
+        {
+        type: 'input',
+        message: 'Installation instructions:',
+        name: 'installation',
+        },
+        {
+        type: 'input',
+        message: 'Usage information:',
+        name: 'usage',
+        },        {
+        type: 'input',
+        message: 'Contribution guidelines:',
+        name: 'contributing',
+        },
+        {
+        type: 'input',
+        message: 'Test instructions:',
+        name: 'tests',
+        },
+        {
+        type: 'list',
+        message: 'License:',
+        name: 'license',
+        },
+        {
+        type: 'input',
+        message: 'GitHub username:',
+        name: 'username',
+        },
+        {
+        type: 'input',
+        message: 'Email address:',
+        name: 'email',
+        }
+    ])
+    .then((response) =>
+
+    );
